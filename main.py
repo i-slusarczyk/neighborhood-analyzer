c677a0e94830cb6b1a3d19d898ff28db6a5bc8df
igorslusarczyk-pc <igorslusarczyk17@gmail.com>
2026-02-20 16:05:01 +0100

    mediana

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from utils import *
 import pandas as pd
 
-mapbox_token = "pk.eyJ1Ijoia2phbHYiLCJhIjoiY21lbWk5N3BqMG9xazJpczlxdTZyb2xueiJ9._l4sMzp9t6JVxx1ESh6EBQ"
-style_id = "light-v11"
-mapbox_url = f"https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/256/{{z}}/{{x}}/{{y}}@2x?access_token={mapbox_token}"
 
-dom = (50.172617, 19.916644)
-lat = dom[0]
-lon = dom[1]
-# cafes = get_cafes(lat, lon)
+def load_data():
+    return gpd.read_parquet(r"data\krakow.parquet")
+
+
+testing_point = (50.066130, 19.921678)
+lat = testing_point[0]
+lon = testing_point[1]
+
 shops = get_poi(lat, lon, tags={"shop": True})
 amenities = get_poi(lat, lon, tags={"amenity": True})
-shops.explore(marker_type="marker", tiles=mapbox_url,
-              attr="© Mapbox © OpenStreetMap", cmap="viridis").save("features.html")
+
 
 markers = pd.concat([shops, amenities])
-markers.explore(marker_type="marker", tiles=mapbox_url,
-                attr="© Mapbox © OpenStreetMap", cmap="viridis").save("features.html")
-print("Success")
+
+median_price = get_flats_nearby(load_data(), lat, lon)
+print(median_price)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import osmnx as ox
 import geopandas as gpd
 import pandas as pd
+import shapely
 
 
 def get_poi(lat: float, lon: float, tags: dict, radius: int = 1000) -> gpd.GeoDataFrame:
@@ -13,3 +14,13 @@ def get_poi(lat: float, lon: float, tags: dict, radius: int = 1000) -> gpd.GeoDa
         return gdf_poi_clean_metric
     except Exception:
         return None
+
+
+def get_flats_nearby(gdf_flats: gpd.GeoDataFrame, lat: float, lon: float, radius: int = 500):
+    gdf_flats = gpd.read_parquet(r"data\krakow.parquet")
+    target_point = shapely.Point(lon, lat)
+    gdf_target_point = gpd.GeoSeries(
+        [target_point], crs="EPSG:4326").to_crs(epsg=2180)
+    distances = gdf_flats["geometry"].distance(gdf_target_point.iloc[0])
+    median_price = gdf_flats.loc[distances <= radius, "pricePerMeter"].median()
+    return median_price
