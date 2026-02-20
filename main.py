fb49dd03edb999a909738461544976c39e33322e
igorslusarczyk-pc <igorslusarczyk17@gmail.com>
2026-02-20 10:32:15 +0100

    s

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import *
+import pandas as pd
 
 mapbox_token = "pk.eyJ1Ijoia2phbHYiLCJhIjoiY21lbWk5N3BqMG9xazJpczlxdTZyb2xueiJ9._l4sMzp9t6JVxx1ESh6EBQ"
 style_id = "light-v11"
@@ -8,6 +9,12 @@ dom = (50.172617, 19.916644)
 lat = dom[0]
 lon = dom[1]
 # cafes = get_cafes(lat, lon)
-shops = get_shops(lat, lon)
+shops = get_poi(lat, lon, tags={"shop": True})
+amenities = get_poi(lat, lon, tags={"amenity": True})
 shops.explore(marker_type="marker", tiles=mapbox_url,
               attr="© Mapbox © OpenStreetMap", cmap="viridis").save("features.html")
+
+markers = pd.concat([shops, amenities])
+markers.explore(marker_type="marker", tiles=mapbox_url,
+                attr="© Mapbox © OpenStreetMap", cmap="viridis").save("features.html")
+print("Success")
--- utils.py
+++ utils.py
@@ -3,24 +3,13 @@ import geopandas as gpd
 import pandas as pd
 
 
-def get_cafes(lat: float, lon: float, radius: int = 1000) -> gpd.GeoDataFrame:
+def get_poi(lat: float, lon: float, tags: dict, radius: int = 1000) -> gpd.GeoDataFrame:
     try:
-        cafes = ox.features_from_point(
-            (lat, lon), dist=radius, tags={"amenity": "cafe"})
-        cafes_clean_metric = cafes.reset_index(
+        gdf_poi = ox.features_from_point(
+            (lat, lon), dist=radius, tags=tags)
+        gdf_poi_clean_metric = gdf_poi.reset_index(
         )[["name", "geometry"]].to_crs(epsg=2180)
-        return cafes_clean_metric
-    except Exception:
-        return None
-
-
-def get_shops(lat: float, lon: float, radius: int = 1000) -> gpd.GeoDataFrame:
-    try:
-        shops = ox.features_from_point(
-            (lat, lon), dist=radius, tags={"shop": True})
-        shops_clean_metric = shops.reset_index(
-        )[["name", "geometry"]].to_crs(epsg=2180)
-        shops_clean_metric["geometry"] = shops_clean_metric["geometry"].centroid
-        return shops_clean_metric
+        gdf_poi_clean_metric["geometry"] = gdf_poi_clean_metric["geometry"].centroid
+        return gdf_poi_clean_metric
     except Exception:
         return None
