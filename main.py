6e25939e2c7049222e714ba26eff97d43af96f84
igorslusarczyk-pc <igorslusarczyk17@gmail.com>
2026-02-21 15:26:46 +0100

    wyświetlanie ikonek na mapie niedokończone

--- main.py
+++ main.py
@@ -2,22 +2,53 @@ from utils import get_poi, get_flats_nearby
 import geopandas as gpd
 import pandas as pd
 import streamlit as st
+import folium
 from streamlit_folium import st_folium
 from pathlib import Path
+from folium.plugins import BeautifyIcon
 
 
 @st.cache_data
-def load_data():
-    return gpd.read_parquet(Path("data") / "krakow_flats.parquet")
+def load_poi():
+    return gpd.read_parquet(Path("data")/"krakow_poi.parquet")
+
+
+@st.cache_data
+def load_nature():
+    return gpd.read_parquet(Path("data")/"krakow_nature.parquet")
+
+
+@st.cache_data
+def load_flats():
+    return gpd.read_parquet(Path("data")/"krakow_flats.parquet")
 
 
 testing_point = (50.066130, 19.921678)
 lat = testing_point[0]
 lon = testing_point[1]
 
+median_price = get_flats_nearby(load_flats(), lat, lon)[
+    "pricePerMeter"].median()
+st.write(f"Mediana ceny za metr w okolicy twojej pinezki to {median_price} zł")
 
-median_price = get_flats_nearby(load_data(), lat, lon)
-st.write(
-    f"Mediana ceny za metr w okolicy twojej pinezki to {median_price} zł")
-mapa = markers.explore(marker_kwds={"radius": '10'})
-st_folium(mapa)
+icons_config = {
+    "cafe": {"icon": "coffee", "color": "darkred"},
+    "kindergarten": {"icon": "child", "color": "blue"},
+    "supermarket": {"icon": "shopping-cart", "color": "green"},
+    "tram_stop": {"icon": "train", "color": "cadetblue"}
+}
+
+ikona_przedszkola = BeautifyIcon(
+    icon="child",
+    icon_shape="circle",  # To usuwa nóżkę pinezki i robi idealne koło
+    border_color="#1E90FF",  # Niebieska obwódka (DodgerBlue)
+    text_color="#1E90FF",  # Kolor samej ikonki
+    background_color="white",  # Tło wewnątrz kółka
+    border_width=2  # Grubość obwódki w pikselach
+)
+
+kindergartens = get_poi(load_poi(), lat, lon, "kindergarten", radius=20000)
+m = kindergartens.explore(marker_type="marker", marker_kwds={
+                          "icon": folium.Icon(icon="child", prefix="fa", color="blue")})
+folium.LayerControl().add_to(m)
+st_folium(m)
--- utils.py
+++ utils.py
@@ -3,16 +3,13 @@ import geopandas as gpd
 import shapely
 
 
-def get_poi(lat: float, lon: float, tags: dict, radius: int = 1000) -> gpd.GeoDataFrame:
-    try:
-        gdf_poi = ox.features_from_point(
-            (lat, lon), dist=radius, tags=tags)
-        gdf_poi_clean_metric = gdf_poi.reset_index(
-        )[["name", "geometry"]].to_crs(epsg=2180)
-        gdf_poi_clean_metric["geometry"] = gdf_poi_clean_metric["geometry"].centroid
-        return gdf_poi_clean_metric
-    except Exception:
-        return None
+def get_poi(gdf_poi: gpd.GeoDataFrame, lat: float, lon: float, category: str, radius: int = 1000) -> gpd.GeoDataFrame:
+    target_point = shapely.Point(lon, lat)
+    gdf_target_point = gpd.GeoSeries(
+        [target_point], crs="EPSG:4326").to_crs(epsg=2180)
+    gdf_poi = gdf_poi.loc[gdf_poi["category"] == category]
+    distances = gdf_poi["geometry"].distance(gdf_target_point.iloc[0])
+    return gdf_poi.loc[distances <= radius]
 
 
 def get_flats_nearby(gdf_flats: gpd.GeoDataFrame, lat: float, lon: float, radius: int = 500):
@@ -20,5 +17,4 @@ def get_flats_nearby(gdf_flats: gpd.GeoDataFrame, lat: float, lon: float, radius
     gdf_target_point = gpd.GeoSeries(
         [target_point], crs="EPSG:4326").to_crs(epsg=2180)
     distances = gdf_flats["geometry"].distance(gdf_target_point.iloc[0])
-    median_price = gdf_flats.loc[distances <= radius, "pricePerMeter"].median()
-    return median_price
+    return gdf_flats.loc[distances <= radius]
