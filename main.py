3c8b3082aaf84885461f62ae9c9d5e94bf3d295a
igorslusarczyk-pc <igorslusarczyk17@gmail.com>
2026-02-23 20:51:47 +0100

    nature view

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from utils import get_poi, get_flats_nearby
 import geopandas as gpd
-import pandas as pd
+from pathlib import Path
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
-from pathlib import Path
-from folium.plugins import BeautifyIcon
+from utils import get_poi, get_flats_nearby, get_nature
 
 
 @st.cache_data
@@ -23,32 +21,45 @@ def load_flats():
     return gpd.read_parquet(Path("data")/"krakow_flats.parquet")
 
 
-testing_point = (50.066130, 19.921678)
-lat = testing_point[0]
-lon = testing_point[1]
+if "pin_lat" not in st.session_state:
+    default_point = (50.066130, 19.921678)
+    st.session_state.pin_lat = default_point[0]
+    st.session_state.pin_lon = default_point[1]
+    st.session_state.map_center_lat = st.session_state.pin_lat
+    st.session_state.map_center_lon = st.session_state.pin_lon
+    st.session_state.map_zoom = 14
+lat = st.session_state.pin_lat
+lon = st.session_state.pin_lon
 
 median_price = get_flats_nearby(load_flats(), lat, lon)[
     "pricePerMeter"].median()
-st.write(f"Mediana ceny za metr w okolicy twojej pinezki to {median_price} zł")
+st.write(
+    f"Mediana ceny w latach 2023-2024 za metr mieszkania w okolicy twojej pinezki to {median_price:.2f} zł")
 
-icons_config = {
-    "cafe": {"icon": "coffee", "color": "darkred"},
-    "kindergarten": {"icon": "child", "color": "blue"},
-    "supermarket": {"icon": "shopping-cart", "color": "green"},
-    "tram_stop": {"icon": "train", "color": "cadetblue"}
-}
 
-ikona_przedszkola = BeautifyIcon(
-    icon="child",
-    icon_shape="circle",  # To usuwa nóżkę pinezki i robi idealne koło
-    border_color="#1E90FF",  # Niebieska obwódka (DodgerBlue)
-    text_color="#1E90FF",  # Kolor samej ikonki
-    background_color="white",  # Tło wewnątrz kółka
-    border_width=2  # Grubość obwódki w pikselach
+local_nature = get_nature(load_nature(), lat, lon)
+m_base = folium.Map(
+    location=[st.session_state.map_center_lat,
+              st.session_state.map_center_lon],
+    zoom_start=st.session_state.map_zoom
 )
+local_nature.explore(m=m_base)
+map_data = st_folium(m_base, key="Green areas", width=800, height=600)
+st.write(local_nature.area.sum())
+st.write(map_data)
 
-kindergartens = get_poi(load_poi(), lat, lon, "kindergarten", radius=20000)
-m = kindergartens.explore(marker_type="marker", marker_kwds={
-                          "icon": folium.Icon(icon="child", prefix="fa", color="blue")})
-folium.LayerControl().add_to(m)
-st_folium(m)
+if map_data and map_data.get("last_clicked"):
+    clicked_lat = map_data["last_clicked"]["lat"]
+    clicked_lon = map_data["last_clicked"]["lng"]
+    if clicked_lat != st.session_state.pin_lat or clicked_lon != st.session_state.pin_lon:
+        st.session_state.pin_lat = clicked_lat
+        st.session_state.pin_lon = clicked_lon
+
+        if map_data.get("center"):
+            st.session_state.map_center_lat = map_data["center"]["lat"]
+            st.session_state.map_center_lon = map_data["center"]["lng"]
+
+        if map_data.get("zoom"):
+            st.session_state.map_zoom = map_data["zoom"]
+
+        st.rerun()
--- utils.py
+++ utils.py
@@ -1,20 +1,43 @@
-import osmnx as ox
 import geopandas as gpd
 import shapely
 
 
+def get_target_point(lat: float, lon: float):
+    return gpd.GeoSeries([shapely.Point(lon, lat)], crs="EPSG:4326").to_crs(epsg=2180)
+
+
 def get_poi(gdf_poi: gpd.GeoDataFrame, lat: float, lon: float, category: str, radius: int = 1000) -> gpd.GeoDataFrame:
-    target_point = shapely.Point(lon, lat)
-    gdf_target_point = gpd.GeoSeries(
-        [target_point], crs="EPSG:4326").to_crs(epsg=2180)
+    target_point = get_target_point(lat, lon)
     gdf_poi = gdf_poi.loc[gdf_poi["category"] == category]
-    distances = gdf_poi["geometry"].distance(gdf_target_point.iloc[0])
+    distances = gdf_poi["geometry"].distance(target_point.iloc[0])
     return gdf_poi.loc[distances <= radius]
 
 
 def get_flats_nearby(gdf_flats: gpd.GeoDataFrame, lat: float, lon: float, radius: int = 500):
-    target_point = shapely.Point(lon, lat)
-    gdf_target_point = gpd.GeoSeries(
-        [target_point], crs="EPSG:4326").to_crs(epsg=2180)
-    distances = gdf_flats["geometry"].distance(gdf_target_point.iloc[0])
+    target_point = get_target_point(lat, lon)
+    distances = gdf_flats["geometry"].distance(target_point.iloc[0])
     return gdf_flats.loc[distances <= radius]
+
+
+def get_nature(gdf_nature: gpd.GeoDataFrame, lat: float, lon: float, radius: int = 800) -> gpd.GeoDataFrame:
+    gdf_target_buffer = get_target_point(lat, lon).buffer(radius)
+    gdf_clipped = gpd.clip(gdf_nature, gdf_target_buffer)
+    return gdf_clipped
+
+
+def nature_score():
+    """NATURE_GLOBAL_WEIGHT = 27.1
+nature_weights = {
+    "water": 0.24,
+    "park": 0.37,
+    "meadow" : 0.08,
+    "grassland": 0.03,
+    "forest": 0.28,
+}
+nature_threshold_area = 0.2*buffer.area
+nature_points = min(((water.area * nature_weights["water"] +
+                    forests.area * nature_weights["forest"] +
+                    parks.area * nature_weights["park"] +
+                    meadows_area * nature_weights["meadow"] +
+                    grassland.aarea * nature_weights["grassland"])/nature_threshold_area), 1
+                    ) * NATURE_GLOBAL_WEIGHT"""
