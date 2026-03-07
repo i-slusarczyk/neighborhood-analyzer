be89f83dd250feae5a944367323af1c4f2f67874
igorslusarczyk-mac <igorslusarczyk17@gmail.com>
2026-03-07 10:22:15 +0100

    hexagon map in streamlit

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def main():
     init_session_state(default_point)
 
     # loading data
+    hex_gdf = load_geodata("data/processed/h3.parquet")
+
     poi_gdf = load_geodata(cfg.POI_PARQUET)
 
     flats_gdf = load_geodata(cfg.FLATS_PARQUET)
@@ -73,44 +75,85 @@ def main():
     layers = result["layers"]
 
     # output
-    if result["median_price"] is not None:
-        st.write(
-            f"Median price for a square meter nearby your pin is {result['median_price']:.2f} zł")
-    else:
-        st.write("Not enough flat offers found nearby")
-    st.write(f"Total base score: {result["base_score"]:.2f}")
-    st.write(f"Final score: {result["final_score"]:.2f}")
 
-    # map rendering
-    m_base = folium.Map(
-        location=[st.session_state.map_center_lat,
-                  st.session_state.map_center_lon],
-        zoom_start=st.session_state.map_zoom,
-        tiles=None
-    )
-    folium.TileLayer(tiles="CartoDB Positron",
-                     name="CartoDB Positron").add_to(m_base)
+    tab1, tab2 = st.tabs(["Place Rating", "Overall Map"])
 
-    if not layers["nature"].empty:
-        layers["nature"].explore(
-            m=m_base,
-            name="Green areas",
-            highlight=True,
-            tooltip=False
+    with tab1:
+        st.markdown("### Scoring Details")
+
+        components = result["component_scores"]
+
+        n_cols = len(components) + 2
+
+        cols = st.columns(n_cols)
+
+        for i, (category_name, value) in enumerate(components.items()):
+            cols[i].metric(label=category_name.capitalize(),
+                           value=f"{value:.1f}")
+        cols[-2].metric(label="Destructors",
+                        value=f"{result["destructors"]:.1f}")
+        cols[-1].metric(label="Total score",
+                        value=f"{result["final_score"]:.1f}")
+        if result["median_price"] is not None:
+            st.subheader(
+                f"Median price for a square meter nearby your pin is {result['median_price']:.0f} zł")
+        else:
+            st.subheader("Not enough flat offers found nearby")
+
+        # map rendering
+        m_base = folium.Map(
+            location=[st.session_state.map_center_lat,
+                      st.session_state.map_center_lon],
+            zoom_start=st.session_state.map_zoom,
+            tiles=None
         )
-    if not layers["transport"].empty:
-        layers["transport"].explore(
-            m=m_base,
-            name="Public transport stops",
-            highlight=True,
-        )
-    folium.Marker(location=(pin_lat, pin_lon)).add_to(m_base)
-    folium.LayerControl(collapsed=False).add_to(m_base)
-    map_data = st_folium(m_base, key="PoI map",
-                         use_container_width=True, height=500)
+        folium.TileLayer(tiles="CartoDB Positron",
+                         name="CartoDB Positron").add_to(m_base)
 
-    # streamlit map interactions
-    handle_map_interactions(map_data)
+        if not layers["nature"].empty:
+            layers["nature"].explore(
+                m=m_base,
+                name="Green areas",
+                highlight=True,
+                tooltip=False
+            )
+        if not layers["transport"].empty:
+            layers["transport"].explore(
+                m=m_base,
+                name="Public transport stops",
+                highlight=True,
+            )
+        folium.Marker(location=(pin_lat, pin_lon)).add_to(m_base)
+        folium.LayerControl(collapsed=False).add_to(m_base)
+        map_data = st_folium(m_base, key="PoI map",
+                             use_container_width=True, height=500)
+
+        handle_map_interactions(map_data)
+
+    with tab2:
+        m_hex = folium.Map(
+            location=[cfg.city_center[1], cfg.city_center[0]], zoom_start=12, tiles=None)
+
+        folium.TileLayer(tiles="CartoDB Positron",
+                         name="CartoDB Positron",).add_to(m_hex)
+
+        m_macro = hex_gdf.assign(
+            final_score=hex_gdf["final_score"].round(2),
+            median_price=hex_gdf["median_price"].round(0),
+            value_ratio=(hex_gdf["value_ratio"]*15).round(2))
+
+        value_ratio_map = m_macro.explore(column="value_ratio", cmap="RdYlGn", tooltip=[
+            "final_score", "median_price", "value_ratio"], m=m_hex, name="Value Ratio", legend=False, show=True)
+        m_macro.explore(column="final_score", cmap="RdYlGn", tooltip=[
+            "final_score", "median_price", "value_ratio"], m=m_hex, name="Final Score", legend=False, show=False)
+        median_price_map = m_macro.explore(column="median_price", cmap="RdYlGn", tooltip=[
+            "final_score", "median_price", "value_ratio"], m=m_hex, name="Median price", legend=False, show=False)
+
+        folium.LayerControl(collapsed=False).add_to(m_hex)
+
+        st_folium(m_hex, use_container_width=True)
+
+    # base map interactions
 
 
 if __name__ == "__main__":
